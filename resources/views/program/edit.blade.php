1d82c6793db859d5115a040baa4ea26e8d38e172
isa9x <iza_helena93@yahoo.co.id>
2015-04-01 17:17:51 +0700

    oi
    oi

--- resources/views/program/edit.blade.php
+++ resources/views/program/edit.blade.php
@@ -2,7 +2,7 @@
 	
 	<div class="modal-header">
   	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                 <h1>Edit: {!! $program-> nik !!} </h1>
+                 <h1>Edit: {!! $program-> nama!!} </h1>
 	
  
    </div>			<!-- /modal-header -->
